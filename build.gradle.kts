66f7b9a345ff6f3ec87b5ec0915dc36c62f91575
Ryan Doherty <tech@conical.org>
2022-06-30 06:55:34 -0400

    Upgrade core

--- build.gradle.kts
+++ build.gradle.kts
@@ -84,7 +84,7 @@ repositories {
 dependencies {
 
   // Core lib
-  implementation("org.veupathdb.lib:jaxrs-container-core:6.7.1")
+  implementation("org.veupathdb.lib:jaxrs-container-core:6.7.2")
 
   // Jersey
   implementation("org.glassfish.jersey.core:jersey-server:3.0.4")
