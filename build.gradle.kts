17559743242ac026558c957597c4dcb5bd09b624
Elizabeth <foxcapade@gmail.com>
2022-07-12 14:18:54 -0400

    update core lib version

--- build.gradle.kts
+++ build.gradle.kts
@@ -84,7 +84,7 @@ repositories {
 dependencies {
 
   // Core lib
-  implementation("org.veupathdb.lib:jaxrs-container-core:6.7.4")
+  implementation("org.veupathdb.lib:jaxrs-container-core:6.8.0")
 
   // Jersey
   implementation("org.glassfish.jersey.core:jersey-server:3.0.4")
