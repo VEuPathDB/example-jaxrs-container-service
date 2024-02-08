840fc026ead84b0b4335e043d371583938bee8d2
Ryan Doherty <tech@conical.org>
2024-02-08 13:43:31 -0500

    lib upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.veupathdb.lib.gradle.container.util.Logger.Level
 
 plugins {
   java
-  id("org.veupathdb.lib.gradle.container.container-utils") version "4.8.9"
+  id("org.veupathdb.lib.gradle.container.container-utils") version "4.8.10"
   id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
@@ -81,14 +81,14 @@ configurations.all {
 dependencies {
 
   // Core lib
-  implementation("org.veupathdb.lib:jaxrs-container-core:6.16.0")
+  implementation("org.veupathdb.lib:jaxrs-container-core:6.21.0")
 
   // Jersey
   implementation("org.glassfish.jersey.core:jersey-server:3.1.1")
 
   // Jackson
-  implementation("com.fasterxml.jackson.core:jackson-databind:2.15.1")
-  implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.1")
+  implementation("com.fasterxml.jackson.core:jackson-databind:2.15.3")
+  implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.3")
 
   // Log4J
   implementation("org.apache.logging.log4j:log4j-api:2.20.0")
