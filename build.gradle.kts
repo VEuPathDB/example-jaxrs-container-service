4f837d22e7743466dcac53f6f5ed8acc8af8a6c9
Ryan Doherty <tech@conical.org>
2024-02-17 15:44:57 -0500

    Upgrade to latest bearer-token-enabled container core

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,7 +81,7 @@ configurations.all {
 dependencies {
 
   // Core lib
-  implementation("org.veupathdb.lib:jaxrs-container-core:7.0.0-alpha10")
+  implementation("org.veupathdb.lib:jaxrs-container-core:7.0.0-alpha13")
 
   // Jersey
   implementation("org.glassfish.jersey.core:jersey-server:3.1.1")
