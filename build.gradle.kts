9f356847f8f015bfd98cffe0d599bf2bc57d08fe
Ryan Doherty <tech@conical.org>
2024-03-02 02:52:58 -0500

    Upgrade to merged container core and enable user querying for startup config error catching

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,7 +81,7 @@ configurations.all {
 dependencies {
 
   // Core lib
-  implementation("org.veupathdb.lib:jaxrs-container-core:7.0.0-beta2")
+  implementation("org.veupathdb.lib:jaxrs-container-core:7.0.0")
 
   // Jersey
   implementation("org.glassfish.jersey.core:jersey-server:3.1.1")
